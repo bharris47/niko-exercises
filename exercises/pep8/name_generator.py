72ab2f9b727c51bd947fcb228f31fc6dde408e00
Ben Harris <benharris247@gmail.com>
2018-10-16 13:26:27 -0700

    add docstring

--- exercises/pep8/name_generator.py
+++ exercises/pep8/name_generator.py
@@ -3,6 +3,10 @@ firstNames =['Bob', 'Alice', 'Frank',  'Ben', 'Niko', 'Vicky', 'Amanda']
 lastNames=['Harris', 'Barretto', 'Hansen',   'Smith']
 
 def getRandomName():
+    """
+    Generates a random name.
+    :return: Full random name as a string.
+    """
     firstName = random.choice(firstNames)
     lastName = random.choice(lastNames)
     return ' '.join([firstName, lastName])
