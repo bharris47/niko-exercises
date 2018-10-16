7eb38e719a5e847326aa58b3132a07d9272523f5
Alfonso-Niko Barretto <abarretto@salesforce.com>
2018-10-16 14:19:19 -0700

    review

--- tests/pep8/test_name_generator.py
+++ tests/pep8/test_name_generator.py
@@ -1,12 +1,12 @@
 import random
 import unittest
 
-from exercises.pep8.name_generator import getRandomName
+from exercises.pep8.name_generator import get_random_name
 
 
 class NameGeneratorTests(unittest.TestCase):
     def test_get_random_name(self):
         random.seed(1)
-        name = getRandomName()
+        name = get_random_name()
         expected = 'Alice Harris'
         self.assertEqual(name, expected)
