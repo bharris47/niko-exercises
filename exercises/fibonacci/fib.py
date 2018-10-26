a8a814b336a256bd1b3b0d9ec506b7b57a96b96a
Alfonso-Niko Barretto <abarretto@salesforce.com>
2018-10-26 13:24:21 -0700

    fib shit

--- exercises/fibonacci/fib.py
+++ exercises/fibonacci/fib.py
@@ -1,7 +1,18 @@
+a = int(input('Give amount: '))
+
+
 def generate_fibonacci(n):
+
     """
     Generate the Fibonnaci sequence up to the n-th number.
 
     :param n: desired length of sequence (e.g. if n is 3, generate 0, 1, 1)
     """
-    pass
+    a, b = 0, 1
+
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+
+print(list(generate_fibonacci(a)))
