32e6508e276eb94544f0bc4fb40cb9054ab8b556
Niko Barretto <abarretto@salesforce.com>
2018-10-26 18:23:46 -0700

    fib shit (#3)
    * fib shit
    
    * Update fib.py
    
    * Update fib.py
    
    * Update fib.py
    
    * Update fib.py
    
    * Update fib.py

--- exercises/fibonacci/fib.py
+++ exercises/fibonacci/fib.py
@@ -1,7 +1,14 @@
+
+
 def generate_fibonacci(n):
+
     """
     Generate the Fibonnaci sequence up to the n-th number.
 
     :param n: desired length of sequence (e.g. if n is 3, generate 0, 1, 1)
     """
-    pass
+    a, b = 0, 1
+
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
