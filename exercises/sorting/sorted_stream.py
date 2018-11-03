2b7bd73f55eedc666313b9ede814706b6285a481
Ben Harris <benharris247@gmail.com>
2018-11-03 10:34:57 -0700

    increase max line length

--- exercises/sorting/sorted_stream.py
+++ exercises/sorting/sorted_stream.py
@@ -1,6 +1,8 @@
 def sorted_stream(*streams):
     """
-    Given a list of streams sorted in ascending order, combine their outputs and produce one fully sorted stream.
+    Given a list of streams sorted in ascending order,
+    combine their outputs and produce one fully sorted stream.
+
      Example:
         given streams are 2 streams producing (1, 3, 5) and (2, 4, 6) respectively,
         the generator returned should produce (1, 2, 3, 4, 5, 6).
